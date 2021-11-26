857522e59ee02e87a2419162be589deb9adce0f3
kykim00 <jeongsaeki@likelion.org>
2021-11-26 20:39:53 +0900

    prac javascript

---
+++ JS/002.js
@@ -0,0 +1,18 @@
+var myVal1 = 'kdkdkdkdkkd' + "아아아아아아";
+
+var myVal2 = 192828393928;
+var myVal3 = [1, 2, 3, 4, 5, 6];
+var myVal4 = { val1: 1, val2: 2 };
+var myVal5 = function () { };
+
+var myVal6 = [1, 2, 3],
+    myVal7 = [4, 5, 6];
+
+
+var myVal8 = "재현선생님";
+var MyVal8 = "호준선생님";
+
+alert(myVal8);
+alert(MyVal8);
+
+var name = "재현선생님";
