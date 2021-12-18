a3898c59d9d87bf8e320a719cbe96f032b45f1ed
kykim00 <jeongsaeki@likelion.org>
2021-12-18 22:44:46 +0900

    기술면접연습

--- 자료구조알고리즘/code_snippet.js
+++ 자료구조알고리즘/code_snippet.js
@@ -369,18 +369,17 @@ let 길이 = 입력값.length;
 
 // 주의해야 할 사항 : pop을 하면 length가 줄어듭니다!
 // for (let i = 0; i < 입력값.length; i++) {
-//     console.log(입력값.pop())    
+//     console.log(입력값.pop())
 //     console.log(i)
 // }
 
 for (let i = 0; i < 길이; i++) {
-    let 최솟값 = Math.min(...입력값);
-    정렬된배열.push(최솟값);
-    입력값.splice(입력값.indexOf(최솟값), 1)
+  let 최솟값 = Math.min(...입력값);
+  정렬된배열.push(최솟값);
+  입력값.splice(입력값.indexOf(최솟값), 1);
 }
 
 console.log(정렬된배열);
-
 
 ///////
 
@@ -389,32 +388,30 @@ let 정렬된배열 = [];
 
 let 길이 = 입력값.length;
 
-
-while(!!입력값.toString()){
-    let 최솟값 = Math.min(...입력값);
-    정렬된배열.push(최솟값);
-    입력값.splice(입력값.indexOf(최솟값), 1)
+while (!!입력값.toString()) {
+  let 최솟값 = Math.min(...입력값);
+  정렬된배열.push(최솟값);
+  입력값.splice(입력값.indexOf(최솟값), 1);
 }
 
 console.log(정렬된배열);
 
-
 // 3.1 선택정렬(메서드 최소화)
 // 제대로 하려면(자리 바꾸는 것까지)
 function selectionSort(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        let min_index = i;
-        for (let j = i + 1; j < arr.length; j++) {
-            if (arr[min_index] > arr[j]) {
-                min_index = j;
-            }
-        }
-        // 자리바꿈
-        let temp = arr[min_index];
-        arr[min_index] = arr[i];
-        arr[i] = temp;
+  for (let i = 0; i < arr.length; i++) {
+    let min_index = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[min_index] > arr[j]) {
+        min_index = j;
+      }
     }
-    return arr;
+    // 자리바꿈
+    let temp = arr[min_index];
+    arr[min_index] = arr[i];
+    arr[i] = temp;
+  }
+  return arr;
 }
 
 const arr = [199, 22, 33, 12, 32, 64, 72, 222, 233];
@@ -437,81 +434,91 @@ let 후 = [22, 33, 199];
 let 전 = [32, 64, 72, 222, 233];
 let 후 = [12, 22, 33, 199];
 
-
 let 입력값 = [199, 22, 33, 12, 32, 64, 72, 222, 233];
 let 정렬된배열 = [];
 let 배열의길이 = 입력값.length;
 
-function 삽입값이들어갈인덱스(정렬된배열, 삽입값){
-    for (const i in 정렬된배열) {
-        if (삽입값 < 정렬된배열[i]) {
-            return i
-        }
+function 삽입값이들어갈인덱스(정렬된배열, 삽입값) {
+  for (const i in 정렬된배열) {
+    if (삽입값 < 정렬된배열[i]) {
+      return i;
     }
-    return 정렬된배열.length;
+  }
+  return 정렬된배열.length;
 }
 
 for (let i = 0; i < 배열의길이; i++) {
-    let 삽입값 = 입력값.shift()
-    let 인덱스 = 삽입값이들어갈인덱스(정렬된배열, 삽입값);
-    정렬된배열.splice(인덱스, 0, 삽입값);
-    console.log(`인덱스 : ${인덱스}\n삽입값 : ${삽입값}\n 정렬된배열 : ${정렬된배열}\n`)
+  let 삽입값 = 입력값.shift();
+  let 인덱스 = 삽입값이들어갈인덱스(정렬된배열, 삽입값);
+  정렬된배열.splice(인덱스, 0, 삽입값);
+  console.log(
+    `인덱스 : ${인덱스}\n삽입값 : ${삽입값}\n 정렬된배열 : ${정렬된배열}\n`
+  );
 }
-
 
 // 좀 더 어렵게
 function insertIndex(sorted_arr, value) {
-    //삽입될 위치를 찾는 함수
-    for(let i in sorted_arr){
-        if(value < sorted_arr[i]){
-            return i;
-        }
+  //삽입될 위치를 찾는 함수
+  for (let i in sorted_arr) {
+    if (value < sorted_arr[i]) {
+      return i;
     }
-    return sorted_arr.length;
+  }
+  return sorted_arr.length;
 }
 
 function insertSort(arr) {
-    let sorted_arr = [];
+  let sorted_arr = [];
 
-    while (arr.length != 0){
-        let value = arr.shift();
-        //삽입될 위치를 반환함
-        let index = insertIndex(sorted_arr, value);
-        //삽입될 위치에 값을 반환
-        sorted_arr.splice(index, 0, value);
-    }
-    return sorted_arr;
+  while (arr.length != 0) {
+    let value = arr.shift();
+    //삽입될 위치를 반환함
+    let index = insertIndex(sorted_arr, value);
+    //삽입될 위치에 값을 반환
+    sorted_arr.splice(index, 0, value);
+  }
+  return sorted_arr;
 }
 const arr = [199, 22, 33, 12, 32, 64, 72, 222, 233];
 console.log(insertSort(arr));
-
 
 // 3.3 병합정렬(Worst와 Best 모두 O(nlogn), 어떤 정렬보다 빠름, 동일 할 수 있음)
 // let 입력값 = [5, 10, 66, 77, 54, 32, 11, 15];
 // let 정렬된배열 = [];
 
 // 분할(이해를 돕기 위해 8개로 조정)
-[5, 10, 66, 77], [54, 32, 11, 15]
-[5, 10], [66, 77], [54, 32], [11, 15]
-[5], [10], [66], [77], [54], [32], [11], [15]
-
-//정복(0번째끼리 비교!)
-[5, 10], [66, 77], [32, 54], [11, 15]
-[5, 10, 66, 77], [11, 15, 32, 54]
-[5, 10, 11, 15, 32, 54, 66, 77]
+[5, 10, 66, 77],
+  [54, 32, 11, 15][(5, 10)],
+  [66, 77],
+  [54, 32],
+  [11, 15][5],
+  [10],
+  [66],
+  [77],
+  [54],
+  [32],
+  [11],
+  [15][
+    //정복(0번째끼리 비교!)
+    (5, 10)
+  ],
+  [66, 77],
+  [32, 54],
+  [11, 15][(5, 10, 66, 77)],
+  [11, 15, 32, 54][(5, 10, 11, 15, 32, 54, 66, 77)];
 
 // step 1
 let 입력값 = [5, 10, 66, 77, 54, 32, 11, 15];
 
-function 병합정렬(입력배열){
-    let 입력배열의길이 = 입력배열.length;
-    if (입력배열의길이 <= 1){
-        return 입력배열
-    }
-    let 중간값 = parseInt(입력배열의길이 / 2);
-    let 그룹하나 = 병합정렬(입력배열.slice(0, 중간값));
-    let 그룹둘 = 병합정렬(입력배열.slice(중간값))
-    return `그룹하나 : ${그룹하나} \n 그룹둘 : ${그룹둘}\n\n`
+function 병합정렬(입력배열) {
+  let 입력배열의길이 = 입력배열.length;
+  if (입력배열의길이 <= 1) {
+    return 입력배열;
+  }
+  let 중간값 = parseInt(입력배열의길이 / 2);
+  let 그룹하나 = 병합정렬(입력배열.slice(0, 중간값));
+  let 그룹둘 = 병합정렬(입력배열.slice(중간값));
+  return `그룹하나 : ${그룹하나} \n 그룹둘 : ${그룹둘}\n\n`;
 }
 
 console.log(병합정렬(입력값));
@@ -520,71 +527,71 @@ console.log(병합정렬(입력값));
 let 입력값 = [5, 10, 66, 77, 54, 32, 11, 15];
 
 function 병합정렬(입력배열) {
-    let 입력배열의길이 = 입력배열.length;
-    let 결과값 = [];
+  let 입력배열의길이 = 입력배열.length;
+  let 결과값 = [];
 
-    if (입력배열의길이 <= 1) {
-        return 입력배열
+  if (입력배열의길이 <= 1) {
+    return 입력배열;
+  }
+
+  let 중간값 = parseInt(입력배열의길이 / 2);
+  let 그룹하나 = 병합정렬(입력배열.slice(0, 중간값));
+  let 그룹둘 = 병합정렬(입력배열.slice(중간값));
+
+  while (그룹하나.length != 0 && 그룹둘.length != 0) {
+    if (그룹하나[0] < 그룹둘[0]) {
+      결과값.push(그룹하나.shift());
+    } else {
+      결과값.push(그룹둘.shift());
     }
+  }
 
-    let 중간값 = parseInt(입력배열의길이 / 2);
-    let 그룹하나 = 병합정렬(입력배열.slice(0, 중간값));
-    let 그룹둘 = 병합정렬(입력배열.slice(중간값));
+  while (그룹하나.length != 0) {
+    결과값.push(그룹하나.shift());
+  }
 
-    while (그룹하나.length != 0 && 그룹둘.length != 0) {
-        if (그룹하나[0] < 그룹둘[0]) {
-            결과값.push(그룹하나.shift());
-        } else {
-            결과값.push(그룹둘.shift());
-        }
-    }
+  while (그룹둘.length != 0) {
+    결과값.push(그룹둘.shift());
+  }
 
-    while(그룹하나.length != 0) {
-        결과값.push(그룹하나.shift());
-    }
-
-    while(그룹둘.length != 0) {
-        결과값.push(그룹둘.shift());
-    }
-
-    return 결과값
+  return 결과값;
 }
 
 console.log(병합정렬(입력값));
 
 // 고급
-function mergeSort(arr){
-    if (arr.length <= 1){
-        return arr;
+function mergeSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const mid = Math.floor(arr.length / 2);
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+  let result = [];
+
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      result.push(left.shift());
+    } else {
+      result.push(right.shift());
     }
+  }
 
-        const mid = Math.floor(arr.length / 2);
-        const left = arr.slice(0, mid);
-        const right = arr.slice(mid);
+  while (left.length) {
+    result.push(left.shift());
+  }
 
-        return merge(mergeSort(left), mergeSort(right));
-    }
+  while (right.length) {
+    result.push(right.shift());
+  }
 
-function merge(left, right){
-    let result = [];
-
-    while (left.length && right.length){
-        if (left[0] < right[0]){
-            result.push(left.shift());
-        } else {
-            result.push(right.shift());
-        }
-    }
-
-    while (left.length) {
-        result.push(left.shift());
-    }
-
-    while (right.length) {
-        result.push(right.shift());
-    }
-
-    return result;
+  return result;
 }
 
 const arr = [199, 22, 33, 12, 32, 64, 72, 222, 233];
@@ -596,73 +603,87 @@ console.log(mergeSort(arr));
 let 입력값 = [66, 77, 54, 32, 10, 5, 11, 15];
 
 //피봇값 : 66
-[54, 32, 10, 5, 11, 15] + [66] + [77]
-
-//피봇값 : 54(66과 77은 값이 한 개이기 때문에 더이상 재귀로 호출되지 않음.)
-[32, 10, 5, 11, 15], [54] + [66] + [77]
-
-//피봇값 : 32
-[10, 5, 11, 15], [32] + [54] + [66] + [77]
-
-//피봇값 : 10 
-[5] + [10], [11, 15] + [32] + [54] + [66] + [77]
-
-//피봇값 : 11
-[5] + [10] + [11] + [15] + [32] + [54] + [66] + [77]
-
+[54, 32, 10, 5, 11, 15] +
+  [66] +
+  [77][
+    //피봇값 : 54(66과 77은 값이 한 개이기 때문에 더이상 재귀로 호출되지 않음.)
+    (32, 10, 5, 11, 15)
+  ],
+  [54] +
+    [66] +
+    [77][
+      //피봇값 : 32
+      (10, 5, 11, 15)
+    ],
+  [32] +
+    [54] +
+    [66] +
+    [77][5] + //피봇값 : 10
+    [10],
+  [11, 15] +
+    [32] +
+    [54] +
+    [66] +
+    [77][5] + //피봇값 : 11
+    [10] +
+    [11] +
+    [15] +
+    [32] +
+    [54] +
+    [66] +
+    [77];
 
 let 입력값 = [66, 77, 54, 32, 10, 5, 11, 15];
-function 퀵정렬(입력배열){
-    let 입력배열의길이 = 입력배열.length;
+function 퀵정렬(입력배열) {
+  let 입력배열의길이 = 입력배열.length;
 
-    if (입력배열의길이 <= 1) {
-        return 입력배열
+  if (입력배열의길이 <= 1) {
+    return 입력배열;
+  }
+
+  const 피벗값 = [입력배열.shift()]; //기준점
+  const 그룹하나 = [];
+  const 그룹둘 = [];
+
+  for (let i in 입력배열) {
+    if (입력배열[i] < 피벗값) {
+      그룹하나.push(입력배열[i]);
+    } else {
+      그룹둘.push(입력배열[i]);
     }
+  }
 
-    const 피벗값 = [입력배열.shift()]; //기준점
-    const 그룹하나 = [];
-    const 그룹둘 = [];
+  console.log(
+    `그룹하나 : ${그룹하나}\n그룹둘 : ${그룹둘}\n피벗값 : ${피벗값}\n`
+  );
 
-    for (let i in 입력배열) {
-        if (입력배열[i] < 피벗값) {
-            그룹하나.push(입력배열[i]);
-        } else {
-            그룹둘.push(입력배열[i]);
-        }
-    }
-
-    console.log(`그룹하나 : ${그룹하나}\n그룹둘 : ${그룹둘}\n피벗값 : ${피벗값}\n`);
-
-    return 퀵정렬(그룹하나).concat(피벗값, 퀵정렬(그룹둘));
-
+  return 퀵정렬(그룹하나).concat(피벗값, 퀵정렬(그룹둘));
 }
 
 console.log(퀵정렬(입력값));
 
-
 // 참고용 Notion 코드
-function quickSort(arr){
-    if (arr.length <= 1){
-        return arr;
-    }
+function quickSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
 
-    const pivot = arr[0]; //기준점
-    const left = [];
-    const right = [];
+  const pivot = arr[0]; //기준점
+  const left = [];
+  const right = [];
 
-    for (let i=1; i<arr.length; i++){
-        if(arr[i] < pivot){
-            left.push(arr[i]);
-        } else {
-            right.push(arr[i]);
-        }
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
     }
-    return quickSort(left).concat(pivot, quickSort(right));
+  }
+  return quickSort(left).concat(pivot, quickSort(right));
 }
 
 const arr = [199, 22, 33, 12, 32, 64, 72, 222, 233];
 console.log(quickSort(arr));
-
 
 // 4. 페이지 교체 알고리즘
 // 페이징 기법으로 메모리를 관리하는 운영체제에서, 페이지 부재가 발생 하여 새로운 페이지를 할당하기 위해 현재 할당된 페이지 중 어느 것과 교체할지를 결정하는 방법이다. 이 알고리즘이 사용되는 시기는 페이지 부재가 발생해 새로운 페이지를 적재 해야하나 페이지를 적재할 공간이 없어 이미 적재되어 있는 페이지 중 교체할 페이지를 정할 때 사용
@@ -693,36 +714,448 @@ console.log(quickSort(arr));
 // miss - 5
 
 //LRU
-["바나나", "체리", "한라봉", "자몽", "수박", "수박", "체리"]
-[바나나] 5
-[바나나, 체리] 5
-[바나나, 체리, 한라봉] 5
-[체리, 한라봉, 자몽] 5
-[한라봉, 자몽, 수박] 5
-[한라봉, 자몽, 수박] 1 - hit
-[자몽, 수박, 체리] 5
-
+// ["바나나", "체리", "한라봉", "자몽", "수박", "수박", "체리"]
+// [바나나] 5
+// [바나나, 체리] 5
+// [바나나, 체리, 한라봉] 5
+// [체리, 한라봉, 자몽] 5
+// [한라봉, 자몽, 수박] 5
+// [한라봉, 자몽, 수박] 1 - hit
+// [자몽, 수박, 체리] 5
 
 // FIFO 알고리즘(캐시:3)
-["바나나", "체리", "한라봉", "자몽", "수박", "수박", "체리"]
-[바나나] 5
-[바나나, 체리] 5
-[바나나, 체리, 한라봉] 5
-[체리, 한라봉, 자몽] 5
-[한라봉, 자몽, 수박] 5 
-[한라봉, 자몽, 수박] 1 - hit
-[자몽, 수박, 체리]  5
+// ["바나나", "체리", "한라봉", "자몽", "수박", "수박", "체리"]
+// [바나나] 5
+// [바나나, 체리] 5
+// [바나나, 체리, 한라봉] 5
+// [체리, 한라봉, 자몽] 5
+// [한라봉, 자몽, 수박] 5
+// [한라봉, 자몽, 수박] 1 - hit
+// [자몽, 수박, 체리]  5
 
 // hit - 1
 // miss - 5
 
 // 5. 트리와 그래프
+const tree = {
+  root: {
+    value: 5,
+    left: {
+      value: 3,
+      left: {
+        value: 1,
+        left: null,
+        right: null,
+      },
+      right: {
+        value: 4,
+        left: null,
+        right: null,
+      },
+    },
+    right: {
+      value: 8,
+      left: {
+        value: 6,
+        left: null,
+        right: null,
+      },
+      right: {
+        value: 9,
+        left: null,
+        right: null,
+      },
+    },
+  },
+};
+
+tree.root.value;
+tree.root.left.value;
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    // this.child = []; // 2진트리가 아닌 트리가 됨
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class Tree {
+  constructor(data) {
+    let init = new Node(data);
+    this.root = init;
+    this.데이터수 = 0;
+  }
+
+  length() {
+    return this.데이터수;
+  }
+
+  insert(data) {
+    let 새로운노드 = new Node(data);
+    let 순회용현재노드 = this.root;
+
+    while (순회용현재노드) {
+      if (data === 순회용현재노드.data) {
+        // 중복된 값은 탈락!
+        return;
+      }
+      if (data < 순회용현재노드.data) {
+        // 들어온 데이터가 작으면 왼쪽에
+        // 비어있으면 데이터를 넣고, 비어있지 않으면 타고 또 내려가야합니다.
+        if (!순회용현재노드.left) {
+          순회용현재노드.left = 새로운노드;
+          return;
+        }
+        순회용현재노드 = 순회용현재노드.left;
+      }
+      if (data > 순회용현재노드.data) {
+        // 들어온 데이터가 크면 오른쪽에
+        // 비어있으면 데이터를 넣고, 비어있지 않으면 타고 또 내려가야합니다.
+        if (!순회용현재노드.right) {
+          순회용현재노드.right = 새로운노드;
+          return;
+        }
+        순회용현재노드 = 순회용현재노드.right;
+      }
+    }
+
+    this.데이터수 += 1;
+  }
+  DFS() {
+    // 깊이우선탐색, DFS(Depth First Search)
+    // Stack 이용!
+    let 결과값 = [];
+    let 스택 = [this.root];
+
+    while (스택.length !== 0) {
+      let current = 스택.pop();
+      if (current.right) {
+        스택.push(current.right);
+      }
+      if (current.left) {
+        스택.push(current.left);
+      }
+      결과값.push(current.data);
+    }
+    return 결과값;
+  }
+
+  BFS() {
+    // 너비우선탐색, DFS(Breadth First Search)
+    // Queue 이용!
+    let 결과값 = [];
+    let 큐 = [this.root];
+
+    while (큐.length !== 0) {
+      let current = 큐.shift();
+      if (current.left) {
+        큐.push(current.left);
+      }
+      if (current.right) {
+        큐.push(current.right);
+      }
+      결과값.push(current.data);
+    }
+    return 결과값;
+  }
+}
+
+let t = new Tree(5); // root노드는 처음에!!
+t.insert(3);
+t.insert(8);
+t.insert(1);
+t.insert(4);
+t.insert(6);
+t.insert(9);
+
+// t.root.data
+// 5
+// t.root.left.data
+// 3
+// t.root.right.right.data
+// 9
+
 // 6. 트리의 순회
-
-
 
 // 목차(실전 코딩테스트 풀이)
 // 1. 18년도
+
+// 주제: 2진법, 진법 연산, replace, or연산
+
+////////////////////
+
+let n = 5;
+let arr1 = [9, 20, 28, 18, 11];
+let arr2 = [30, 1, 21, 17, 28];
+
+function solution(n, arr1, arr2) {
+  let result = [];
+  for (let i = 0; i < n; i++) {
+    result.push(
+      (arr1[i] | arr2[i]).toString(2).replace(/1/g, "#").replace(/0/g, " ")
+    );
+  }
+  return result;
+}
+
+console.log(solution(n, arr1, arr2));
+
+////////////////////
+
+// 유틸리티 모듈
+
+const zip = (a, b) => a.map((value, index) => [value, b[index]]);
+
+const fillZero = (n, arr) => {
+  return "0".repeat(n - arr.length) + arr;
+};
+
+////
+
+let n = 5;
+let arr1 = [9, 20, 28, 18, 11];
+let arr2 = [30, 1, 21, 17, 28];
+
+function solution(n, arr1, arr2) {
+  let result = [];
+  // const zip = (a, b) => a.map((value, index)=>[value, b[index]]);
+  const fillSpace = (n, arr) => {
+    return " ".repeat(n - arr.length) + arr;
+  };
+  for (let i = 0; i < n; i++) {
+    result.push(
+      fillSpace(
+        n,
+        (arr1[i] | arr2[i]).toString(2).replace(/1/g, "#").replace(/0/g, " ")
+      )
+    );
+  }
+  return result;
+}
+
+console.log(solution(n, arr1, arr2));
+
+////
+
+let n = 5;
+let arr1 = [9, 20, 28, 18, 11];
+let arr2 = [30, 1, 21, 17, 28];
+
+function solution(n, arr1, arr2) {
+  let result = [];
+  const zip = (a, b) => a.map((value, index) => [value, b[index]]);
+  const fillSpace = (n, arr) => {
+    return " ".repeat(n - arr.length) + arr;
+  };
+  for (let [i, j] of zip(arr1, arr2)) {
+    result.push(
+      fillSpace(n, (i | j).toString(2).replace(/1/g, "#").replace(/0/g, " "))
+    );
+  }
+  return result;
+}
+
+console.log(solution(n, arr1, arr2));
+
+////
+// 다트 게임
+// https://tech.kakao.com/2017/09/27/kakao-blind-recruitment-round-1/
+// 문자열 파싱(Parsing)
+
+testcase = ["1S2D*3T", "1D2S#10S", "1D2S0T"];
+
+for (const c of testcase) {
+  console.log(solution(c));
+}
+
+// 37, 9, 3
+
+//step1
+const dartResult = "1S2D3T";
+let answer = [];
+let result = 0;
+let temp = 0; // 임시점수
+
+for (let i = 0; i < dartResult.length; i++) {
+  // console.log(dartResult[i]);
+  if (dartResult[i] >= 0 && dartResult[i] <= 9) {
+    temp = parseInt(dartResult[i]);
+  } else if (dartResult[i] == "S") {
+    answer.push(temp);
+  } else if (dartResult[i] == "D") {
+    // answer.push(Math.pow(temp, 2));
+    answer.push(temp ** 2);
+  } else if (dartResult[i] == "T") {
+    // answer.push(Math.pow(temp, 3));
+    answer.push(temp ** 3);
+  }
+}
+
+console.log(answer);
+
+//step2
+const dartResult = "1D2S#10S";
+let answer = [];
+let result = 0;
+let temp = 0; // 임시점수
+
+for (let i = 0; i < dartResult.length; i++) {
+  // console.log(dartResult[i]);
+  if (dartResult[i] >= 0 && dartResult[i] <= 9) {
+    if (dartResult[i] == 1 && dartResult[i + 1] == 0) {
+      temp = 10;
+      i++;
+    } else {
+      temp = parseInt(dartResult[i]);
+    }
+  } else if (dartResult[i] == "S") {
+    answer.push(temp);
+  } else if (dartResult[i] == "D") {
+    // answer.push(Math.pow(temp, 2));
+    answer.push(temp ** 2);
+  } else if (dartResult[i] == "T") {
+    // answer.push(Math.pow(temp, 3));
+    answer.push(temp ** 3);
+  } else if (dartResult[i] == "*") {
+    answer[answer.length - 1] *= 2;
+    answer[answer.length - 2] *= 2;
+  } else if (dartResult[i] == "#") {
+    answer[answer.length - 1] *= -1;
+  }
+}
+for (let i = 0; i < answer.length; i++) {
+  result += answer[i];
+}
+
+console.log(answer);
+
+// step3
+function solution(dartResult) {
+  let answer = [];
+  let result = 0;
+  let temp = 0; // 임시점수
+
+  for (let i = 0; i < dartResult.length; i++) {
+    // console.log(dartResult[i]);
+    if (dartResult[i] >= 0 && dartResult[i] <= 9) {
+      if (dartResult[i] == 1 && dartResult[i + 1] == 0) {
+        temp = 10;
+        i++;
+      } else {
+        temp = parseInt(dartResult[i]);
+      }
+    } else if (dartResult[i] == "S") {
+      answer.push(temp);
+    } else if (dartResult[i] == "D") {
+      // answer.push(Math.pow(temp, 2));
+      answer.push(temp ** 2);
+    } else if (dartResult[i] == "T") {
+      // answer.push(Math.pow(temp, 3));
+      answer.push(temp ** 3);
+    } else if (dartResult[i] == "*") {
+      answer[answer.length - 1] *= 2;
+      answer[answer.length - 2] *= 2;
+    } else if (dartResult[i] == "#") {
+      answer[answer.length - 1] *= -1;
+    }
+  }
+  for (let i = 0; i < answer.length; i++) {
+    result += answer[i];
+  }
+  return result;
+}
+
+//////////
+// https://tech.kakao.com/2017/09/27/kakao-blind-recruitment-round-1/
+// 캐시문제
+// 키워드 : LRU 알고리즘, 페이지 교체 알고리즘
+// 3	["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"]	50
+// 3	["Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul"]	21
+// 2	["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"]	60
+
+testcase = [
+  [
+    3,
+    [
+      "Jeju",
+      "Pangyo",
+      "Seoul",
+      "NewYork",
+      "LA",
+      "Jeju",
+      "Pangyo",
+      "Seoul",
+      "NewYork",
+      "LA",
+    ],
+  ],
+  [
+    3,
+    [
+      "Jeju",
+      "Pangyo",
+      "Seoul",
+      "Jeju",
+      "Pangyo",
+      "Seoul",
+      "Jeju",
+      "Pangyo",
+      "Seoul",
+    ],
+  ],
+  [
+    2,
+    [
+      "Jeju",
+      "Pangyo",
+      "Seoul",
+      "NewYork",
+      "LA",
+      "SanFrancisco",
+      "Seoul",
+      "Rome",
+      "Paris",
+      "Jeju",
+      "NewYork",
+      "Rome",
+    ],
+  ],
+];
+
+for (const [cacheSize, cities] of testcase) {
+  console.log(solution(cacheSize, cities));
+}
+
+// 50, 21, 60
+
+function solution(cacheSize, cities) {
+  let time = 0;
+  let cache = [];
+  for (let i = 0; i < cities.length; i++) {
+    let city = cities[i].toLowerCase();
+    let index = cache.indexOf(city);
+    if (index !== -1) {
+      // hit
+      cache.splice(index, 1);
+      cache.push(city);
+      time += 1;
+    } else {
+      // miss
+      time += 5;
+      cache.push(city);
+      if (cacheSize < cache.length) {
+        cache.shift();
+      }
+    }
+  }
+  return time;
+}
+
+// 2. 19년도
+// 3. 20년도
+// 4. 21년도
 // 2. 19년도
 // 3. 20년도
 // 4. 21년도
